e854c12cf7ff422fc1f5307e10aa1b633a18600e
Levi Lavengood <123432087+Lwalkl@users.noreply.github.com>
2023-06-14 14:36:36 -0400

    Update Spectrogram_Resolution_Test.m

--- PressureAnalysis/Spectrogram_Resolution_Test.m
+++ PressureAnalysis/Spectrogram_Resolution_Test.m
@@ -12,26 +12,24 @@ D2= 'June 08, 2022 15:10:00';
 
 ind = find(time>=t1&time<=t2+1/16/86400); % Framing data to match time frame
 time0 = time(ind);
+seconds = (time0-time0(1))*86400;
 P0 = P(ind);
 P1= detrend(P0);
 
 fs= 16.66;      % Sample Rate 
 N= length(ind);
 
-Ts = [2,2.5,3,4,6] ;
-dTs = [.1, .25,.5,.75,.9] ;
+Ts = [6,8,12,15,20,30] ; % in seconds
+dTs = 0.5 ; %in seconds
 for ii= 1:length(Ts)
     T= Ts(ii);
     L= round(T*fs);
-    for jj= 1:length(dTs)
-        dT= dTs(jj);
-        Olap= round(dT*L);
+        Olap= L-round(dTs*fs);
         [s, f, tfs]=spectrogram(P1,L,Olap,0:fs/N:3,fs,"yaxis"); 
         Ns = length(f);
-        Fig(jj) = figure;
-        Fig(jj).Name = sprintf ('Spectorgram L%d Olap%d',L,Olap);
+        Fig = figure;
+        Fig.Name = sprintf ('Spectorgram L%d Olap%d',L,Olap);
         imagesc(tfs,f,(s.*conj(s)/Ns*fs));
-        set(gca,'xlim',[500 575],'ylim',[0 2.6]);
-        saveas(gcf,sprintf('Figure%d.fig',ii,jj))
-    end
+        set(gca,'ylim',[0 1.5]);
+        saveas(gcf,sprintf('2ndFigure%d.fig',ii))
 end
