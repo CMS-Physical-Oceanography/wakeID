45538cd615568a53210373920115ddf70e4661f2
Levi Lavengood <123432087+Lwalkl@users.noreply.github.com>
2023-06-15 09:25:14 -0400

    Update Spectrogram_Resolution_Test.m
    Updated version

--- PressureAnalysis/Spectrogram_Resolution_Test.m
+++ PressureAnalysis/Spectrogram_Resolution_Test.m
@@ -1,12 +1,12 @@
 % This set of code will give you a set of different resolutions based on
 % different window sizes and overlap lengths based on time and sample rate.
-
+clear all;
 close all;
 
 load ('processed_hatchery_june2022.mat',"time", "P")
 
 D1= 'June 08, 2022 15:00:00';     % Time frame for data to be analyzed 
-D2= 'June 08, 2022 15:10:00';
+D2= 'June 08, 2022 15:30:00';
     t1= datenum(D1);
     t2= datenum(D2);
 
@@ -19,17 +19,26 @@ P1= detrend(P0);
 fs= 16.66;      % Sample Rate 
 N= length(ind);
 
-Ts = [6,8,12,15,20,30] ; % in seconds
-dTs = 0.5 ; %in seconds
+Ts = [8,16,32, 64] ;
+dTs = 0.5 ;
 for ii= 1:length(Ts)
     T= Ts(ii);
     L= round(T*fs);
         Olap= L-round(dTs*fs);
-        [s, f, tfs]=spectrogram(P1,L,Olap,0:fs/N:3,fs,"yaxis"); 
+        [s, f, tfs]=spectrogram(P1,hamming(L),Olap,0:fs/L:3,fs,"yaxis"); 
         Ns = length(f);
+        S = (s.*conj(s)/(L*fs));
         Fig = figure;
         Fig.Name = sprintf ('Spectorgram L%d Olap%d',L,Olap);
-        imagesc(tfs,f,(s.*conj(s)/Ns*fs));
+        imagesc(tfs,f,S); caxis([0 0.0001])
+        colormap(flipud(bone))
         set(gca,'ylim',[0 1.5]);
         saveas(gcf,sprintf('2ndFigure%d.fig',ii))
+        if ~exist('figVar','var')
+            figVar = figure;
+        end
+        figure(figVar), hold on
+        varS = sum(S*(fs/L),1);
+        plot(tfs,varS)
+        var(ii) = sum(varS*dTs)
 end
